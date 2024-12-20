f842f5b36517e47e9a522651100ea7f8af39fe92
Berivan AKYILDIZ <berivan.akyildiz0@gmail.com>
2024-12-20 14:47:38 +0300

    Add files via upload

---
+++ 3_first_app.py
@@ -0,0 +1,35 @@
+maas = 100000
+vergi = 0.27
+print(maas - (maas*vergi))
+
+# ------------
+
+print("Maasiniz:",7000 - (7000 * 0.27))
+
+# --------
+
+maas = input("Maasinizi girin:")
+vergi = 0.27
+toplam = (float(maas) - (float(maas)*vergi))
+print(toplam)
+
+
+adi = "Melih"
+soyadi = "KOCA"
+cinsiyeti = "Erkek"
+tc_kimlik = "11111111111"
+dogum_yili = 1995
+yasi = 2024 - dogum_yili
+adres = "Malatya/Türkiye"
+print(adi , soyadi , cinsiyeti , tc_kimlik , dogum_yili , yasi , adres)
+
+
+#------------------------------------------------
+
+siparis1 = 110 
+sipari2 = 1100.5
+siparis3 = 356.95
+
+toplam = siparis1 + sipari2 + siparis3
+print("Tutar:" , toplam)
+#--------------------------------------------------
